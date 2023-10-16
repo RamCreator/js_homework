a73a252ad2a6426a9f5bf9c3278993a8c6500693
RamCreator <rammaye87540644@gmail.com>
2023-10-16 17:42:28 +0530

    operator

---
+++ operator.js
@@ -0,0 +1,40 @@
+x = 10;
+y = 5;
+z = x + y
+console.log(z);
+
+z = x * y
+console.log(z);
+
+z = x ** y
+console.log(z);
+
+z = x / y
+console.log(z);
+
+z = x % y
+console.log(z);
+
+z = x - y
+console.log(z);
+
+x = 35
+console.log(x);
+
+x +=  1;
+console.log(x);
+
+x -= 1 ;
+console.log(x);
+
+
+a =3;
+b=9; 
+console.log(a==b); 
+console.log(a===b); 
+console.log(a>b); 
+console.log(a<b); 
+console.log(a>=b); 
+console.log(a<=b); 
+console.log(a!=b); 
+ console.log(a!==b); 
