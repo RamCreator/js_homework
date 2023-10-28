4b516708e69e64366ea97ea2abb17f012d9e52e5
RamCreator <rammaye87540644@gmail.com>
2023-10-28 21:21:28 +0530

    new one

---
+++ lcu.js
@@ -0,0 +1,16 @@
+let lcu = ["Kaithi", "Vikram", "Leo"];
+
+// variable in movie name
+let movie1 = "kaithi";
+let movie2 = "vikram";
+let movie3 = "leo";
+let movie4 = "RRR";
+
+const lcuMovies =prompt("movie name:")
+
+// chek the movie in lcu
+if (lcu.includes(lcuMovies)) {
+  console.log(lcuMovies,"is in the LCU.");
+} else {
+  console.log(lcuMovies,"is not in the LCU.");
+}
